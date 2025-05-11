fdbc9748711c56f6dd266dfe444c2d20c8140835
Maheshwarirahul612 <rahulbadga51@gmail.com>
2025-05-11 06:12:25 -0700

    second  commit

--- server.js
+++ server.js
@@ -64,5 +64,5 @@ socketHandler(io);
 // Start the server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, '0.0.0.0',() => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
