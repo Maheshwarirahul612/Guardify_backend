6ba4871021f70b673c0aa5a786f4378c75f2aa1b
Maheshwarirahul612 <rahulbadga51@gmail.com>
2025-05-13 06:21:31 -0700

    first commit

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads')));
 socketHandler(io);
 
 // Start the server
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
